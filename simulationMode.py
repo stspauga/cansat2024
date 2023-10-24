7ff30a03a476325ebc33c6f4e48e189a052b7c45
tpauga <tpauga@asu.edu>
2023-10-24 15:42:08 -0700

    Loop though and ignore comments

--- simulationMode.py
+++ simulationMode.py
@@ -1,5 +1,9 @@
 
 
 
-simFile = open("cansat_2023_simp.txt","r")
+simFile = open("cansat_2023_simp.txt","r",encoding="utf-8")
+for line in simFile:
+    if "#" not in line:
+        print(line, end='')
 
+simFile.close()
