721103fcca1d0875daa376b292e64e60a11e0bf6
Tuliloa Pauga <tuliloapauga@Tuliloas-MacBook-Pro.local>
2024-03-28 15:22:14 -0700

    Initial Commit

--- writeToCSV.py
+++ writeToCSV.py
@@ -1,9 +1,6 @@
-import time
 import pandas as pd
-from datetime import datetime
 import time
-import math 
-from geopy.geocoders import Nominatim
+import functions
 
 
 
@@ -28,70 +25,6 @@ df = pd.read_csv('cansat_test.csv')
 
 
 
-#--------------FUNCTIONS------------------#
-def calcAltitude(airPressure):
-    pressure_value = float(airPressure[-1])
-    power = (1 / 5.255)
-    base = (pressure_value / 101326)
-    altitude = (44330 * (1 - (base ** power))) - 633.19
-    if altitude < 0:
-        return 0.0
-    return str(round(altitude, 2))
-    
-    
-def calculateCurrentTime():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return current_time
-    
-def calculateTime(startTime):
-    elapsedSeconds = round(time.time() - startTime, 0)
-    minutes = 0
-    elapsedTime = f"{minutes}:{elapsedSeconds}"
-    if elapsedSeconds % 60 == 0 and not elapsedSeconds == 0.0:
-        minutes = minutes + 1
-        elapsedSeconds = round(time.time() - startTime, 0)
-        elapsedTime = f"{minutes}:{elapsedSeconds}"
-
-    return elapsedTime
-
-def determineState():
-    #Need to change obviously but for now this is it
-    return "IN FLIGHT"
-    
-#THIS FUNCTION IS VERY WRONG NEED TO FIX
-#NOT TAKING SPEED FROM PREVIOUS
-#COORDINATE, ONLY TAKING SPEED FROM 0,0 BASICALLY
-def calcAirSpeed(stringAlt, horizontalDistance):
-    altitude = float(stringAlt)
-    trueDistance = math.sqrt(pow(altitude, 2) + pow(horizontalDistance, 2))
-    elapsedSeconds = time.time() - startTime   
-    if elapsedSeconds == 0:
-        return "No speed yet"
-    return round(float(trueDistance) / float(elapsedSeconds), 2)
-
-def calcLatandLon():
-    # calling the Nominatim tool and create Nominatim class
-    loc = Nominatim(user_agent="Geopy Library")
-
-    # entering the location name
-    getLoc = loc.geocode("Arizona")
-
-    # printing address
-    #print(getLoc.address)
-
-    def returnLat():
-        #print("Latitude = ", getLoc.latitude, "\n")
-        return getLoc.latitude
-
-    def returnLon():
-        #print("Longitude = ", getLoc.longitude)
-        return getLoc.longitude
-
-    return returnLat, returnLon
-
-
-
 #----------OPENING AND WRITING TO CSV FILE------------#
 simFile = open("cansat_2023_simp.txt", "r", encoding="utf-8")
 rowNum = 0
@@ -106,12 +39,12 @@ for line in simFile:
         #Assigning returns from functions to their respective variables
         split_string = line.split(",")
         pressure = split_string[-1].strip().split()
-        altitude = calcAltitude(pressure)
-        GPSTime = calculateCurrentTime()
-        MissionTime = calculateTime(startTime)
-        state = determineState()
-        airSpeed = calcAirSpeed(str(altitude),dummyHorizontal)
-        returnLat, returnLon = calcLatandLon()
+        altitude = functions.calcAltitude(pressure)
+        GPSTime = functions.calculateCurrentTime()
+        MissionTime = functions.calculateTime(startTime)
+        state = functions.determineState()
+        airSpeed = functions.calcAirSpeed(str(altitude),dummyHorizontal, startTime)
+        returnLat, returnLon = functions.calcLatandLon()
         lat = returnLat()
         lon = returnLon()
         
