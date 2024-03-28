812368b4069ba86cd58b3af3ed5b3002f3fcb95b
stspauga <116019616+stspauga@users.noreply.github.com>
2024-03-28 15:04:07 -0700

    Add formatted data (#4)
    * A commit
    
    * Initial Commit
    
    * Added a few data points and implemented dummy data
    
    * Initial Commit
    
    * Added dummy data and functions
    
    * Another quick change
    
    ---------
    
    Co-authored-by: Tuliloa Pauga <tuliloapauga@tuliloas-mbp.dhcp.asu.edu>
    Co-authored-by: Tuliloa Pauga <tuliloapauga@Tuliloas-MacBook-Pro.local>

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ from dash import dcc, html, Input, Output, callback
 import pandas as pd
 import plotly.express as px
 
+#rowNum = 1
 # Load data from CSV file
 data = pd.read_csv('cansat_test.csv')
 
@@ -12,6 +13,37 @@ app = dash.Dash(__name__)
 
 def serve_layout():
     return html.H1('The time is: ' + str(datetime.datetime.now()))
+
+
+
+@app.callback(
+    Output('live-update-graph','figure'),
+    [Input('interval-component','n_intervals')]
+)
+def update_graph(n):
+    #global rowNum
+    
+    #Reading the CSV data into "updated_data"
+    updated_data = pd.read_csv('cansat_test.csv')
+
+    # if rowNum >= len(updated_data) - 1:
+    #     rowNum = 0  # Reset rowNum to 0 or another appropriate value based on your needs
+    # else:
+    #     rowNum += 1
+        
+    #Updating the line
+    fig = px.line(updated_data, x='Mission Time', y='Altitude',title='Update Graph')
+
+    #Updating the max and min
+    # xMax = updated_data.loc[rowNum,"Mission Time"]
+    # yMax = updated_data.loc[rowNum,"Altitude"]
+
+    fig.update_layout(
+        xaxis=dict(range=[0, 40]),  # Set x-axis range from 0 to 1000
+        yaxis=dict(range=[0, 1400]),  # Set y-axis range from 0 to 1000
+    )
+
+    return fig
 
 
 # Define layout
@@ -24,21 +56,6 @@ app.layout = html.Div([
         ),
 ])
 
-@app.callback(
-    Output('live-update-graph','figure'),
-    [Input('interval-component','n_intervals')]
-)
-def update_graph(n):
-    updated_data = pd.read_csv('cansat_test.csv')
-
-    fig = px.line(updated_data, x='Time', y='Altitude',title='Update Graph')
-
-    fig.update_layout(
-        xaxis=dict(range=[0, 200]),  # Set x-axis range from 0 to 1000
-        yaxis=dict(range=[0, 1400]),  # Set y-axis range from 0 to 1000
-    )
-
-    return fig
 
 # Run the app
 if __name__ == '__main__':
